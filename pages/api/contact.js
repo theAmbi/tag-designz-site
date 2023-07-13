262c41e8ff3155299f51a740293a756173280aa6
Golden Ekpendu <ekpendugolden1@gmail.com>
2023-07-13 18:41:06 +0100

    changed contact.js to commonjs syntax

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,11 +1,45 @@
-import { mailOptions, transporter } from '@/config/nodemailer';
+// import { mailOptions, transporter } from '@/config/nodemailer';
+
+// const handler = async (req, res) => {
+//     if (req.method === "POST") {
+//         const data = req.body;
+//         if (!data.firstName || !data.email || !data.message) {
+//             return res.status(400).json({ message: 'Bad request' });
+//         };
+//         try {
+//             await transporter.sendMail({
+//                 ...mailOptions,
+//                 subject: "New Message for TAG Designz",
+//                 text: "this is a test string",
+//                 html: `<h1>New email from TAG Designz website</h1>
+//                         <p>${data.message}</p>
+//                         <p>Message sent from ${data.firstName} by ${data.email}</p>
+//                         `
+//             });
+//             return res.status(200).json({ message: 'Email sent successfully!' });
+//         } catch (error) {
+//             console.log(error);
+//             return res.status(400).json({ message: error.message });
+
+//         }
+
+//     }
+
+
+// }
+
+// module.exports = handler;
+
+// commonjs syntax
+
+const { mailOptions, transporter } = require('@/config/nodemailer');
 
 const handler = async (req, res) => {
     if (req.method === "POST") {
         const data = req.body;
         if (!data.firstName || !data.email || !data.message) {
             return res.status(400).json({ message: 'Bad request' });
-        };
+        }
         try {
             await transporter.sendMail({
                 ...mailOptions,
@@ -20,12 +54,8 @@ const handler = async (req, res) => {
         } catch (error) {
             console.log(error);
             return res.status(400).json({ message: error.message });
-
         }
-
     }
-
-
-}
+};
 
 module.exports = handler;
